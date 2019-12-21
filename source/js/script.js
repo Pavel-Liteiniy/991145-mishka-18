57e077fdf876a79aac5d1023a4406f26c9498fd1
Pavel <paulsev96@gmail.com>
2019-12-21 12:48:47 +0300

    Делает блок modal интерактивным

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+// Отображает и скрывает мобильное меню
 var nav = document.querySelector('.header__nav');
 var button = document.querySelector('.header__button');
 
@@ -10,5 +11,33 @@ button.addEventListener('click', function () {
   } else {
     nav.classList.add('header__nav--closed');
     nav.classList.remove('header__nav--opened');
+  }
+});
+
+// Отображает и скрывает модальное окно
+var link = document.querySelector(".button--modal");
+var modal = document.querySelector(".modal");
+var inputRadio = modal.querySelector("[id=choose-size-s]");
+var overlay = document.querySelector(".modal__overlay");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal--show");
+  overlay.classList.add("modal__overlay--show");
+  inputRadio.focus();
+});
+
+overlay.addEventListener("click", function (evt) {
+  modal.classList.remove("modal--show");
+  overlay.classList.remove("modal__overlay--show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modal.classList.contains("modal--show")) {
+      modal.classList.remove("modal--show");
+      overlay.classList.remove("modal__overlay--show");
+    }
   }
 });

5f2d5b013ea6c0a5566478da7a828dc1ee751fa1
Pavel <paulsev96@gmail.com>
2020-01-22 21:33:45 +0300

    Добавляет интерактивную карту Яндекс

--- source/js/script.js
+++ source/js/script.js
@@ -43,3 +43,37 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+// Интерактивная карта Яндекс
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+    center: [59.938747, 30.323093],
+    zoom: 16
+  }, {
+    searchControlProvider: 'yandex#search'
+  }),
+
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'Собственный значок метки',
+      balloonContent: 'г. Санкт-Петербург, ул. Большая Конюшенная, д. 19/8, офис 101'
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: '../img/icon-map-pin.svg',
+      // Размеры метки.
+      iconImageSize: [66, 101],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-30, -101]
+    })
+
+  myMap.geoObjects
+    .add(myPlacemark);
+});
